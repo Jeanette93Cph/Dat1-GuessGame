458bd46b1d7eee99b5054df028e199302d7f398e
Jeanette93Cph <jeanette.93@live.dk>
2022-11-01 11:16:09 +0100

    changes

--- src/MainFeauture.java
+++ src/MainFeauture.java
@@ -1,5 +1,15 @@
+import java.util.Random;
+
 public class MainFeauture
 {
+
+    public static void main(String[] args)
+    {
+        Random rnd = new Random();
+        System.out.println(rnd);
+    }
+
+
 
     @Override
     public String toString()
--- src/NewFeature.java
+++ src/NewFeature.java
@@ -1,5 +1,14 @@
+import java.util.Random;
+
 public class NewFeature
 {
+
+
+    public static void main(String[] args)
+    {
+        Random rnd = new Random();
+        System.out.println(rnd);
+    }
 
 
     @Override
